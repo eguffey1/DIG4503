17122ee98846b851fc9838a42eed55a0d67e1960
Erickson Guffey <eguffeyarmy2@gmail.com>
2020-04-16 09:28:16 -0400

    commit docs

---
+++ docs/precache-manifest.39e75f213a95ab825aa75f8aab4b34cc.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfea3fed32ca54f8ed2dd1b777f9f4b3",
+    "url": "/DIG4503/index.html"
+  },
+  {
+    "revision": "c400098328bc69bf6eca",
+    "url": "/DIG4503/static/css/2.50bb061f.chunk.css"
+  },
+  {
+    "revision": "40724518d8caf13121e1",
+    "url": "/DIG4503/static/css/main.27919384.chunk.css"
+  },
+  {
+    "revision": "c400098328bc69bf6eca",
+    "url": "/DIG4503/static/js/2.2f06d179.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/DIG4503/static/js/2.2f06d179.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40724518d8caf13121e1",
+    "url": "/DIG4503/static/js/main.82ca4c10.chunk.js"
+  },
+  {
+    "revision": "45185143420156b6c733",
+    "url": "/DIG4503/static/js/runtime-main.e5735d54.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/DIG4503/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/DIG4503/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/DIG4503/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/DIG4503/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/DIG4503/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
